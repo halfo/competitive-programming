2132873cc2457681184af2322b62f8a8eda0766e
Hasib Billah <billah.hasib@yahoo.com>
2013-06-03 21:33:24 +0600

    type-casted all numerical values to long long

--- Number_Theory/Prime_Factorization/__src/numOfDivisor.cpp
+++ Number_Theory/Prime_Factorization/__src/numOfDivisor.cpp
@@ -1,17 +1,17 @@
 long long numOfDivisor (long long n) {
-    if (n < 1) return 0;
+    if (n < 1LL) return 0LL;
     long long cnt = 0LL;
     long long ret = 1LL;
 
-    while (!(n & 1)) { n >>= 1; ++cnt; }
+    while (!(n & 1LL)) { n >>= 1LL; ++cnt; }
     ret *= (cnt + 1LL);
 
-    for (long long i = 3; i * i <= n; i += 2) {
+    for (long long i = 3LL; i * i <= n; i += 2LL) {
         cnt = 0LL;
         while (!(n % i)) { n /= i; ++cnt; }
         ret *= (cnt + 1LL);
     }
 
-    if (n > 1) ret <<= 1;
+    if (n > 1LL) ret <<= 1LL;
     return ret;
 }

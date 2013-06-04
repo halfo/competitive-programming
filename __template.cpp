2f9f6f506d0ee2e9626ec49189ccdc912243ec25
Hasib Billah <billah.hasib@yahoo.com>
2013-06-04 22:01:11 +0600

    C++ Template
    algorithm ::
    runtime   ::
    source    :: shiplu vaia and ienzam vaia
    tested    :: yes

---
+++ __template.cpp
@@ -0,0 +1,76 @@
+//{ ************[       Template       ]************
+using namespace std;
+//{ ************[      C headers       ]************
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cfloat>
+#include <cctype>
+#include <cassert>
+#include <ctime>
+//}
+//{ ************[     C++ headers      ]************
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <string>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <complex>
+#include <tr1/regex>
+#include <tr1/unordered_set>
+#include <tr1/unordered_map>
+//}
+//{ ************[        Loops         ]************
+#define forab(i, a, b) for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i, n) forab (i, 0, (n) - 1)
+#define For(i, n) forab (i, 1, n)
+#define rofba(i, a, b) for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i, n) rofba (i, 0, (n) - 1)
+#define rof(i, n) rofba (i, 1, n)
+#define forstl(i, s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+//}
+//{ ************[   Floating points    ]************
+#define EPS DBL_EPSILON
+#define abs(x) (((x) < 0) ? - (x) : (x))
+#define zero(x) (abs (x) < EPS)
+#define equal(a,b) (zero ((a) - (b)))
+#define PI 2 * acos (0.0)
+//}
+//{ ************[        Macros        ]************
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define all(a) ((a).begin (), (a).end ())
+#define mem(a, i) memset ((a), i, sizeof ((a)))
+#define clr(a) mem ((a), 0)
+#define pb push_back
+//}
+//{ ************[  Typedefs && Consts  ]************
+typedef long long int64;
+typedef unsigned long long int64u;
+
+const int MAX_N = int (1e6) + 7;
+const int MOD   = int (1e9) + 7;
+//}
+//}
+
+int main () {
+	cin.sync_with_stdio (false);
+#ifdef IOfromFile
+	freopen ("input.txt", "r", stdin);
+	// freopen ("output.txt", "w", stdout);
+#endif
+
+	return 0;
+}
+

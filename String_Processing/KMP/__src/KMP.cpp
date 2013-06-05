21428aab252631ff18bc7ac2a687b8b2eff63fb6
Hasib Billah <billah.hasib@yahoo.com>
2013-06-05 18:25:46 +0600

    Added Knuth-Morris-Pratt Algorithm
    algorithm :: knuth-morris-pratt string matching algorithm
    runtime   :: O (n)
    source    :: "http://e-maxx.ru/algo/prefix_function"
    tested    :: yes

---
+++ String_Processing/KMP/__src/KMP.cpp
@@ -0,0 +1,29 @@
+char str [MAX_N], pat [MAX_N];
+int N, M, pi [MAX_N];
+vector <int> occurance;
+
+int failureFunction () {
+	int i, shift = 0;
+	for (i = 2; pat [i - 1]; ++i){
+		while (shift > 0 && pat [shift] != pat [i - 1]) shift = pi [shift];
+		if (pat [shift] == pat [i - 1]) ++shift;
+		pi [i] = shift;
+	}
+	return i - 1;
+}
+
+int KMP () {
+	int i, shift = 0;
+    occurance.clear ();
+	M = failureFunction ();
+
+	for (i = 0; str [i]; ++i) {
+		while (shift > 0 && pat [shift] != str [i] ) shift = pi [shift];
+		if (pat [shift] == str [i]) ++shift;
+		if (shift == M) {
+			occurance.push_back (i - M + 1);
+			shift = pi [shift];
+		}
+	}
+	return i;
+}

340b6cb208ccd76acd9c5130b2dccd7a461a123e
Hasib Billah <billah.hasib@yahoo.com>
2013-06-05 18:18:34 +0600

    Added Failure Function
    algorithm :: failure function or prefix function (equivalent of
    z-algorithm)
    runtime   :: O (n)
    source    :: "http://e-maxx.ru/algo/prefix_function"
    tested    :: yes

---
+++ String_Processing/KMP/__src/failureFunction.cpp
@@ -0,0 +1,12 @@
+char pat [MAX_N];
+int  pi  [MAX_N];
+
+int failureFunction () {
+	int i, shift = 0;
+	for (i = 2; pat [i - 1]; ++i){
+		while (shift > 0 && pat [shift] != pat [i - 1]) shift = pi [shift];
+		if (pat [shift] == pat [i - 1]) ++shift;
+		pi [i] = shift;
+	}
+	return i - 1;
+}

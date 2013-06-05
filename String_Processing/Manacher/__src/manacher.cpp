4886f44980b48a3a465b5cb9b81fb03b4df6b7f2
Hasib Billah <billah.hasib@yahoo.com>
2013-06-05 18:39:19 +0600

    Added Manacher's Algorithm
    algorithm :: manacher's algorithm
    runtime   :: O (n)
    source    :: "http://e-maxx.ru/algo/palindromes_count"
    tested    :: yes

---
+++ String_Processing/Manacher/__src/manacher.cpp
@@ -0,0 +1,28 @@
+char str [MAX_N], modStr [MAX_N << 1];
+int  pal [MAX_N << 1];
+
+int preProcess () {
+    int len = 2;
+    modStrcpy (modStr, "^#");
+
+    for (int i = 0; str [i]; i++) {
+        modStr [len++] = str [i];
+        modStr [len++] = '#';
+    }
+    modStr [len++] = '$';
+    modStr [len++] = 0;
+
+    return len;
+}
+
+void manacher () {
+    int C = 0, R = 0;
+    int N = preProcess ();
+
+    for (int i = 1; i < N - 1; ++i) {
+        int i_mirror = 2 * C - i;
+        pal [i] = (R > i) ? min (R - i, pal [i_mirror]) : 0;
+        while (modStr [i + 1 + pal [i]] == modStr [i - 1 - pal [i]]) ++pal [i];
+        if (i + pal [i] > R) C = i, R = i + pal [i];
+    }
+}

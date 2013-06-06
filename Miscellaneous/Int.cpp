aaf329360ceb821942eefb0b17051669d25c529a
Hasib Billah <billah.hasib@yahoo.com>
2013-06-06 17:57:56 +0600

    Alined 2 Lines

--- Miscellaneous/Int.cpp
+++ Miscellaneous/Int.cpp
@@ -16,8 +16,8 @@ struct Int {
     Int  &operator *= (const Int &rhs)       { return (*this) = *this * rhs; }
     Int  &operator /= (const Int &rhs)       { return (*this) = *this / rhs; }
 
-    bool  operator <  (const Int &rhs) const { return x < rhs.x; }
-    bool  operator >  (const Int &rhs) const { return x > rhs.x; }
+    bool  operator <  (const Int &rhs) const { return x <  rhs.x; }
+    bool  operator >  (const Int &rhs) const { return x >  rhs.x; }
     bool  operator <= (const Int &rhs) const { return x <= rhs.x; }
     bool  operator >= (const Int &rhs) const { return x >= rhs.x; }
     bool  operator != (const Int &rhs) const { return x != rhs.x; }

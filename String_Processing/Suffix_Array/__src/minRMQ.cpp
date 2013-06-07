67c4b37e516f0b5827ba0d2af8891ecf7feefedb
Hasib Billah <billah.hasib@yahoo.com>
2013-06-07 22:39:01 +0600

    RMQ For Finding Minimum (LCP) In Range
    algorithm :: rmq for finding lcp of two prefix
    runtime   :: building -> O (n), GetMin -> O (log n)
    source    :: WJMZBMR
    tested    :: no

---
+++ String_Processing/Suffix_Array/__src/minRMQ.cpp
@@ -0,0 +1,27 @@
+int st [MAX_N << 2];
+
+struct RMQ {
+    int Leaf;
+    RMQ () {
+        Leaf = nextPowOfTwo ();
+        for (int i = 0; i < Leaf; ++i) st [Leaf + i] = i < N ? LCP [i] : INT_MAX;
+        for (int i = Leaf - 1; i > 0; --i) st [i] = min (st [i << 1], st [i << 1 | 1]);
+    }
+
+    int GetMin (int L, int R) const {
+        int res = INT_MAX;
+        if (L > R) swap (L, R);
+        for (L += Leaf, R += Leaf + 1; L < R; L >>= 1, R >>= 1) {
+            if ((L & 1)) res = min (res, st[L]), L++;
+            if ((R & 1)) --R, res = min(res, st[R]);
+        }
+        return res;
+    }
+
+    int nextPowOfTwo () {
+        if (!(N & (N - 1))) return N;
+        Leaf = N | (N - 1);
+        while (Leaf & (Leaf + 1)) Leaf |= (Leaf + 1);
+        return Leaf + 1;
+    }
+};

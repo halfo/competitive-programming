56edb952409fda661e56a82899dada7f68eb08c7
Hasib Billah <billah.hasib@yahoo.com>
2013-06-09 05:18:48 +0600

    Got Rid of NextPowOfTwo Function

--- String_Processing/Suffix_Array/__src/minRMQ.cpp
+++ String_Processing/Suffix_Array/__src/minRMQ.cpp
@@ -3,7 +3,7 @@ int st [MAX_N << 2];
 struct RMQ {
     int Leaf;
     RMQ () {
-        Leaf = nextPowOfTwo ();
+        Leaf = N > 0 ? pow (2, ceil (log10 (N) * 3.3219280946)) : 0;
         for (int i = 0; i < Leaf; ++i) st [Leaf + i] = i < N ? LCP [i] : INT_MAX;
         for (int i = Leaf - 1; i > 0; --i) st [i] = min (st [i << 1], st [i << 1 | 1]);
     }
@@ -16,12 +16,5 @@ struct RMQ {
             if ((R & 1)) --R, res = min (res, st[R]);
         }
         return res;
-    }
-
-    int nextPowOfTwo () {
-        if (!(N & (N - 1))) return N;
-        Leaf = N | (N - 1);
-        while (Leaf & (Leaf + 1)) Leaf |= (Leaf + 1);
-        return Leaf + 1;
     }
 };

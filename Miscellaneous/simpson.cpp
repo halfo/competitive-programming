007f44793f50aac410a816b48e3d08aeb69d8669
Hasib Billah <billah.hasib@yahoo.com>
2013-06-10 01:38:23 +0600

    Added Integration By Simpson's Formula
    algorithm :: integration by simpson's formula
    runtime   :: O (n)
    source    :: "http://e-maxx.ru/algo/simpson_integrating"
    tested    :: no

---
+++ Miscellaneous/simpson.cpp
@@ -0,0 +1,12 @@
+inline double F (double x) {}
+double simpson (double a, double b) {
+    double ret = 0;
+    double N   = int (1e6); // N must be even
+    double h   = (b - a) / N;
+
+    for (int i = 0; i <= N; ++i) {
+        static double x = a + h * i;
+        ret += F (x) * ((i == 0 || i == N) ? 1 : (i & 1) ? 4 : 2);
+    }
+    return ret;
+}

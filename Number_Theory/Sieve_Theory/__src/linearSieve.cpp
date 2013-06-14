8387875ea4a7f48209067255f60f4ae1b1ca5756
Hasib Billah <billah.hasib@yahoo.com>
2013-06-14 09:45:14 +0600

    Revert "Minor Chnage For Safety"
    This reverts commit da092c05df2883277e85653419c994f9f5c0b7c1.

--- Number_Theory/Sieve_Theory/__src/linearSieve.cpp
+++ Number_Theory/Sieve_Theory/__src/linearSieve.cpp
@@ -3,9 +3,7 @@ vector <int> prime;
 
 void linearSieve () {
     flag [4] = 1;
-    prime.clear ();
     prime.push_back (2);
-
     for (int i = 3; i <= MAX_N; i += 2) {
         if (!flag [i]) flag [i] = true, prime.push_back (i);
         for (int j = 0; j < (int) prime.size () && i * prime [j] <= MAX_N; ++j) flag [i * prime [j]] = true;

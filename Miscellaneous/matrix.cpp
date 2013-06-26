787a15afac4d52cc0771b072cfabf6c873a1534d
Hasib Billah <billah.hasib@yahoo.com>
2013-06-26 08:04:31 +0600

    Added Matrix Exponentiation
    algorithm :: matrix exponentiation
    runtime   :: O (Odr ^ 3 log n)
    source    ::
    "http://zobayer.blogspot.com/2010/11/matrix-exponentiation.html"
    tested    :: yes

---
+++ Miscellaneous/matrix.cpp
@@ -0,0 +1,52 @@
+template <int Odr> struct matrix {
+    int64 M [Odr][Odr];
+
+    matrix () { memset (M, 0, sizeof M); }
+    matrix (int64 temp [Odr][Odr]) { memcpy (M, temp, sizeof M); }
+
+    void toIdentity () {
+        memset (M, 0, sizeof M);
+        for (int i = 0; i < Odr; ++i) M [i][i] = 1LL;
+    }
+    void allOne () {
+        for (int i = 0; i < Odr; ++i)
+            for (int j = 0; j < Odr; ++j) M [i][j] = 1LL;
+    }
+
+    matrix <Odr> operator * (const matrix <Odr> &mul) const {
+        matrix <Odr> temp;
+        for (int i = 0; i < Odr; ++i)
+            for (int j = 0; j < Odr; ++j)
+                for (int k = 0; k < Odr; ++k)
+                    temp.M [i][j] += (M [i][k] * mul.M [k][j]);
+        return temp;
+    }
+
+    matrix <Odr> operator % (const int64 mod) const {
+        matrix <Odr> temp;
+        for (int i = 0; i < Odr; ++i)
+            for (int j = 0; j < Odr; ++j)
+                temp.M [i][j] = M [i][j] % mod;
+        return temp;
+    }
+
+    matrix <Odr> pow (int64 power) {
+        matrix <Odr> temp;
+        matrix <Odr> base = *this;
+        temp.toIdentity ();
+        while (power) {
+            if (power & 1LL) temp = (temp * base) % MOD;
+            base = (base * base) % MOD;
+            power >>= 1LL;
+        }
+        return temp % MOD;
+    }
+
+    friend ostream &operator << (ostream &ostr, matrix <Odr> &temp) {
+        for (int i = 0; i < Odr; ++i) {
+            if (i) cout << endl;
+            for (int j = 0; j < Odr; ++j) ostr << temp.M [i][j] << " ";
+        }
+        return ostr;
+    }
+};

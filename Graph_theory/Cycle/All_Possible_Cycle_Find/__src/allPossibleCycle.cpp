fa62bb4c9207dec933520c2b9dc6fbcfc2fc8b07
Hasib Billah <billah.hasib@yahoo.com>
2013-07-21 05:13:15 +0700

    Johnson's Algorithm For All Possible Cycle
    algorithm :: johnson's algorithm (improved by hawick & james) for
    finding all possible cycle in any graph
    runtime   :: O ((N + E) (C + 1))
    source    ::
    "http://blog.mister-muffin.de/2012/07/04/enumerating-elementary-circuits-of-a-directed_graph"
    tested    :: no

---
+++ Graph_theory/Cycle/All_Possible_Cycle_Find/__src/allPossibleCycle.cpp
@@ -0,0 +1,38 @@
+bool seen [MAX_N];
+int nV, cur, nCycle;
+vector <int> stk, adjList [MAX_N], tempList [MAX_N];
+
+inline bool inList (vector <int> &uList, int v) { forstl (it, uList) if (*it == v) return true; return false; }
+bool notSeen (int u) { seen [u] = false; forstl (v, tempList [u]) if (seen [*v]) notSeen (*v); tempList [u].clear (); }
+
+bool calCycle (int u) {
+	bool isCycle = false;
+	seen [u] = true;
+	stk.push_back (u);
+
+	forstl (v, adjList [u]) {
+		if (*v < cur) continue;
+		if (*v == cur) {
+			// print or copy the nodes of cycle, length histogram, longest cycle, vertex popularity
+			nCycle += (isCycle = true);
+		} else if (!seen [*v]) isCycle = calCycle (*v);
+	}
+
+	if (isCycle) notSeen (u);
+	else forstl (v, adjList [u]) {
+		if (*v < cur) continue;
+		if (!inList (tempList [*v], u)) tempList [*v].push_back (u);
+	}
+
+	stk.pop_back ();
+	return isCycle;
+}
+
+void  hawickJames () {
+	stk.clear ();
+	for (cur = nCycle = 0; cur < nV; ++cur) {
+        memset (seen, false, sizeof seen);
+        for (int i = 0; i < MAX_N; ++i) tempList [i].clear ();
+        calCycle (cur);
+    }
+}

0b3574d393adf1b0bccfcdb1a2024e032fb45178
Hasib Billah <billah.hasib@yahoo.com>
2013-07-21 05:15:35 +0700

    Aligned Some Lines

--- __template.cpp
+++ __template.cpp
@@ -34,26 +34,26 @@ using namespace std;
 //}
 //}
 //{ ************[        Loops         ]************
-#define forab(i, a, b) for (__typeof(b) i = (a); i <= (b); ++i)
-#define rep(i, n) forab (i, 0, (n) - 1)
-#define For(i, n) forab (i, 1, n)
-#define rofba(i, a, b) for (__typeof(b) i = (b); i >= (a); --i)
-#define per(i, n) rofba (i, 0, (n) - 1)
-#define rof(i, n) rofba (i, 1, n)
-#define forstl(i, s) for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
+#define forab(i, a, b)	for (__typeof(b) i = (a); i <= (b); ++i)
+#define rep(i, n)		forab (i, 0, (n) - 1)
+#define For(i, n)		forab (i, 1, n)
+#define rofba(i, a, b)	for (__typeof(b) i = (b); i >= (a); --i)
+#define per(i, n)		rofba (i, 0, (n) - 1)
+#define rof(i, n)		rofba (i, 1, n)
+#define forstl(i, s)	for (__typeof ((s).end ()) i = (s).begin (); i != (s).end (); ++i)
 //}
 //{ ************[   Floating points    ]************
-#define EPS DBL_EPSILON
-#define abs(x) (((x) < 0) ? - (x) : (x))
-#define zero(x) (abs (x) < EPS)
-#define equal(a,b) (zero ((a) - (b)))
-#define PI 2 * acos (0.0)
+#define EPS				DBL_EPSILON
+#define abs(x)			(((x) < 0) ? - (x) : (x))
+#define zero(x)			(abs (x) < EPS)
+#define equal(a,b)		(zero ((a) - (b)))
+#define PI				2 * acos (0.0)
 //}
 //{ ************[        Macros        ]************
-#define mem(a, i) memset ((a), i, sizeof ((a)))
-#define clr(a) mem ((a), 0)
-#define all(a) (a).begin (), (a).end ()
-#define pb push_back
+#define mem(a, i)		memset ((a), i, sizeof ((a)))
+#define clr(a)			mem ((a), 0)
+#define all(a)			(a).begin (), (a).end ()
+#define pb				push_back
 
 template <class T, class U> inline T max (T &a, U &b) { return a > b ? a : b; }
 template <class T, class U> inline T min (T &a, U &b) { return a < b ? a : b; }

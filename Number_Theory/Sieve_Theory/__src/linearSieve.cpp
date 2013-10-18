76a66d18431a06fde2ce413ab8ab4dfeb0be9c39
Hasib Billah <billah.hasib@yahoo.com>
2013-10-18 15:51:13 +0700

    Changed Vector's Name From 'prime' To 'primes'

--- Number_Theory/Sieve_Theory/__src/linearSieve.cpp
+++ Number_Theory/Sieve_Theory/__src/linearSieve.cpp
@@ -1,10 +1,10 @@
 bool flag [MAX_N + 1];
-vector <int> prime;
+vector <int> primes;
 
 void linearSieve () {
-    prime.push_back (2);
+    primes.push_back (2);
     for (int i = 3; i <= MAX_N; i += 2) {
-        if (!flag [i]) prime.push_back (i);
-        for (int j = 1; j < (int) prime.size () && i * prime [j] <= MAX_N; ++j) flag [i * prime [j]] = true;
+        if (!flag [i]) primes.push_back (i);
+        for (int j = 1; j < (int) primes.size () && i * primes [j] <= MAX_N; ++j) flag [i * primes [j]] = true;
     }
 }

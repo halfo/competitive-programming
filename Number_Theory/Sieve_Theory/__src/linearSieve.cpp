ce794e9c44c694a5657cd08162d13bd3d145902f
Hasib Billah <billah.hasib@yahoo.com>
2013-10-18 11:44:33 +0700

    Minor Changes

--- Number_Theory/Sieve_Theory/__src/linearSieve.cpp
+++ Number_Theory/Sieve_Theory/__src/linearSieve.cpp
@@ -2,10 +2,9 @@ bool flag [MAX_N + 1];
 vector <int> prime;
 
 void linearSieve () {
-    flag [4] = 1;
     prime.push_back (2);
     for (int i = 3; i <= MAX_N; i += 2) {
-        if (!flag [i]) flag [i] = true, prime.push_back (i);
-        for (int j = 0; j < (int) prime.size () && i * prime [j] <= MAX_N; ++j) flag [i * prime [j]] = true;
+        if (!flag [i]) prime.push_back (i);
+        for (int j = 1; j < (int) prime.size () && i * prime [j] <= MAX_N; ++j) flag [i * prime [j]] = true;
     }
 }
--- String_Processing/KMP/__src/failureFunction.cpp
+++ String_Processing/KMP/__src/failureFunction.cpp
@@ -3,7 +3,7 @@ int  pi  [MAX_N];
 
 int failureFunction () {
 	int i, shift = 0;
-	for (i = 2; pat [i - 1]; ++i){
+	for (i = 2; pat [i - 1]; ++i) {
 		while (shift > 0 && pat [shift] != pat [i - 1]) shift = pi [shift];
 		if (pat [shift] == pat [i - 1]) ++shift;
 		pi [i] = shift;

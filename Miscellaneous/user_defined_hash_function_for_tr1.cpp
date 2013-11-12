a2bea05f7526ff0f5dbbc8fe91d66bbb14536d25
Hasib Billah <billah.hasib@yahoo.com>
2013-11-12 20:27:28 +0600

    User Defined Hash Function For tr1

---
+++ Miscellaneous/user_defined_hash_function_for_tr1.cpp
@@ -0,0 +1,28 @@
+// user defiend hash function for unordered_set and unordered_map
+// to avoid collision, use another hash function along with this one
+
+struct elfHash {
+    // the return type always has to be size_t
+    size_t operator () (const string &key) const {
+        size_t g, h = 0;
+        for (size_t i = 0; key [i]; ++i) {
+            h = (h << 4) + key [i];
+            g = h & 0xf0000000L;
+            if (g) h ^= (g >> 24);
+            h &= ~g;
+        }
+        return h;
+    }
+};
+
+int main () {
+    string fruits [] = {"mango", "orange", "banana", "blueberry"};
+    tr1::unordered_set <string, elfHash> s;
+    tr1::unordered_map <string, int, elfHash> m;
+
+    rep (i, 4) s.insert (fruits [i]), m [fruits [i]] = rand ();
+
+    foreach (i, s) cout << *i << endl; cout << endl;
+    foreach (i, m) cout << i -> first << " " << i -> second << endl;
+    return 0;
+}
